e02f17345a92fc7f094e1eb9ad78c3cc36e1016f
h552hgg <h552hgg@gmail.com>
2021-03-02 13:11:42 -0600

    add a graph

--- static/js/graph.js
+++ static/js/graph.js
@@ -49,33 +49,50 @@ var layout = {
 Plotly.newPlot("plotJon", traceData, layout);
 
 //////////////////////////////////////////////
-// Private tutition comparison 
+// Enrollment Comparison 
+console.log(dataJon2)
+var trace5 = {
+    x: dataJon2.map(row => row.four_year),
+    y: dataJon2.map(row => row.four_public_enrollment),
+    text: dataJon2.map(row => row.four_year),
+    name: "Four Year(Public Enrollment)",
+    type: "bar"
+};
 
-// var trace3 = {
-//     x: data.map(row => row.four_end),
-//     y: data.map(row => row.four_private_tuition),
-//     text: data.map(row => row.four_end),
-//     name: "Four Year",
-//     type: "bar"
-// };
+
+var trace6 = {
+    x: dataJon2.map(row => row.two_year),
+    y: dataJon2.map(row => row.two_public_enrollment),
+    text: dataJon2.map(row => row.two_year),
+    name: "Two Year(Public Enrollment)",
+    type: "bar"
+};
+var trace7 = {
+    x: dataJon2.map(row => row.four_year),
+    y: dataJon2.map(row => row.four_public_enrollment),
+    text: dataJon2.map(row => row.four_year),
+    name: "Four Year(Private Enrollment)",
+    type: "bar"
+};
 
 
-// var trace4 = {
-//     x: data.map(row => row.two_end),
-//     y: data.map(row => row.two_private_tuition),
-//     text: data.map(row => row.two_end),
-//     name: "Two Year",
-//     type: "bar"
-// };
+var trace8 = {
+    x: dataJon2.map(row => row.two_year),
+    y: dataJon2.map(row => row.two_private_enrollment),
+    text: dataJon2.map(row => row.two_year),
+    name: "Two Year(Private Enrollment)",
+    type: "bar"
+};
 
-// // Combining both traces
-// var traceData_1 = [trace3, trace4];
 
-// // Apply the group barmode to the layout
-// var layout_1 = {
-//     title: "Private Tuition",
-//     barmode: "group"
-// };
+// Combining both traces
+var traceData2 = [trace5, trace6, trace7, trace8];
 
-// // Render the plot to the div tag with id "plot"
-// Plotly.newPlot("plot_2", traceData_1, layout_1);
+// Apply the group barmode to the layout
+var layout2 = {
+    title: "Public & Private Enrollment",
+    barmode: "group"
+};
+
+// Render the plot to the div tag with id "plot"
+Plotly.newPlot("plotJon2", traceData2, layout2);
